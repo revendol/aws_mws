b61281f591d819fd02f38f15368181e302403965
oscar-goplexn <150014197+oscar-goplexn@users.noreply.github.com>
2024-07-30 13:15:28 +0600

    setup added

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,36 +1,37 @@
 import { Request, Response } from "express";
 import ProductRepo from "@repos/ProductRepo";
-import {success} from "@shared/response";
+import { success } from "@shared/response";
 import ErrorMessage from "@shared/errorMessage";
 import ProductService from "@services/ProductService";
 
 import StatusCode from "http-status-codes";
-const {OK, INTERNAL_SERVER_ERROR} = StatusCode;
+const { OK, INTERNAL_SERVER_ERROR } = StatusCode;
 import { paginate } from "@util/paginate";
+
 class ProductController {
   async getFeeds(req: Request, res: Response) {
     try {
-        const data = await ProductService.allFeeds();
-        res.status(OK).json(data);
-    } catch (error) {
-        res.status(INTERNAL_SERVER_ERROR).json({ error: error.message });
-    }
-}
-    async createProduct(req: Request, res: Response) {
-        try {
-          const { data } = req.body;
-          await ProductService.addProduct(data);
-          res.status(200).send("Data Added Successfully.");
-      } catch (error) {
-        res.status(500).send("Error Adding Data");
-      }
-    }
-    async getProducts(req: Request, res: Response) {
-      const { page, size, type }: any = req.query;
-      let data: any = await ProductService.allProducts();
+      const data = await ProductService.allFeeds();
       res.status(OK).json(data);
+    } catch (error) {
+      res.status(INTERNAL_SERVER_ERROR).json({ error: error.message });
     }
-    
+  }
+  async createProduct(req: Request, res: Response) {
+    try {
+      const { data } = req.body;
+      await ProductService.addProduct(data);
+      res.status(200).send("Data Added Successfully.");
+    } catch (error) {
+      res.status(500).send("Error Adding Data");
+    }
+  }
+  async getProducts(req: Request, res: Response) {
+    const { page, size, type }: any = req.query;
+    let data: any = await ProductService.allProducts();
+    res.status(OK).json(data);
+  }
+
 }
 
 export default new ProductController();
